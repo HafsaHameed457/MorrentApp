71886c95a1352655f377a50d32434b5ada161967
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-11-30 22:02:46 +0500

    store persistor

--- client/store/index.ts
+++ client/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/rootReducer";
+import { persistStore } from "redux-persist";
 
 const store = configureStore({
   reducer: rootReducer,
 });
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export { store, persistor };
