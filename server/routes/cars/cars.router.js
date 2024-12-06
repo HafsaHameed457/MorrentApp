e901ed6c3d2616785814e0f8949df202318c29b1
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-06 23:21:42 +0500

    crash resolved

--- server/routes/cars/cars.router.js
+++ server/routes/cars/cars.router.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { addNewCar, getAllCars, updateCarFavorite } from "./cars.controller.js";
 const carsRouter = express.Router();
 carsRouter.get("/", () => {});
 carsRouter.post("/", () => {});
 
-carsRouter.post("/favorite", updateCarFavorite);
+carsRouter.post("/favorite", () => {});
 
 export default carsRouter;
