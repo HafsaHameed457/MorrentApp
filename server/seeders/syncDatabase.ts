01f8ffcd4d49bcf2c71818a806ba77b70fc696e7
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-12-07 23:59:02 +0500

    file updated

--- server/seeders/syncDatabase.ts
+++ server/seeders/syncDatabase.ts
@@ -2,7 +2,7 @@ import sequelize from "../models/sequelize";
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync();
+    await sequelize.sync({ force: false });
     console.log("Database synchronized!");
   } catch (error) {
     console.error("Error syncing database:", error);
